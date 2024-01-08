3a4a4bda890515f4562e1bbf2a226ca7613be46f
Alexandros Tsagkaropoulos <alextsagas2001@gmail.com>
2024-01-08 03:09:23 +0200

    add contracs from key.h and key.c to only key.h

--- key.c
+++ key.c
@@ -26,18 +26,6 @@ struct key_t {
   void *data;
 };
 
-/*
-Description: Creates a struct with its data denoted by function's parameter.
-
-Parameters:
-  - data (void *): The data of the struct.
-
-Return values:
-  - key_tp: The pointer to the struct created.
-
-Asserts:
-  - malloc returns successfully.
-*/
 key_tp create_key(void *data) {
   key_tp struct1 = (key_tp)malloc(sizeof(struct key_t));
   assert(struct1 != NULL);
@@ -47,96 +35,30 @@ key_tp create_key(void *data) {
   return struct1;
 }
 
-/*
-Description: Prints the data of the struct with the terminal color that
-the first parameter of the function indicates.
-
-Parameters:
-  - term_color (const char *): The terminal color.
-  - struct1 (key_tp): The struct to be printed.
-  - printer (void (*)(const void *)): The pointer to the function that prints
-    the data of the struct.
-*/
 void print_data(char *term_color,
                 key_tp struct1,
                 void (*printer)(const char *, const void *)) {
   (*printer)(term_color, (void *)struct1->data);
 }
-/*
-Description: Asserts that data in struct1 and struct2 structs are equal.
 
-Parameters:
-  - struct1 (key_tp): The first struct.
-  - struct2 (key_tp): The second struct.
-  - equalizer (int (*)(const void *, const void *)): The pointer to the
-    function that compares the data of the two structs.
-
-Return values:
-    - 1: The data in the two structs are equal.
-    - 0: The data in the two structs are not equal.
-*/
 int are_equal(key_tp struct1,
               key_tp struct2,
               int (*equalizer)(const void *, const void *)) {
   return (*equalizer)((void *)struct1->data, (void *)struct2->data);
 }
 
-/*
-Description: Asserts that data in struct1 is greater than data in struct2.
-
-Parameters:
-  - struct1 (key_tp): The first struct.
-  - struct2 (key_tp): The second struct.
-  - comparator_greater (int (*)(const void *, const void *)): The pointer to the
-    function that compares the data of the two structs.
-
-Return values:
-    - 1: The data in the first struct is greater than the data in the second
-         struct.
-    - 0: The data in the first struct is not greater than the data in the
-         second struct.
-*/
 int is_greater(key_tp struct1,
                key_tp struct2,
                int (*comparator_greater)(const void *, const void *)) {
   return (*comparator_greater)((void *)struct1->data, (void *)struct2->data);
 }
 
-/*
-Description: Asserts that data in struct1 is smaller than data in struct2.
-
-Parameters:
-  - struct1 (key_tp): The first struct.
-  - struct2 (key_tp): The second struct.
-  - comparator_smaller (int (*)(const void *, const void *)): The pointer to the
-    function that compares the data of the two structs.
-
-Return values:
-    - 1: The data in the first struct is smaller than the data in the second
-         struct.
-    - 0: The data in the first struct is not smaller than the data in the
-         second struct.
-*/
 int is_smaller(key_tp struct1,
                key_tp struct2,
                int (*comparator_smaller)(const void *, const void *)) {
   return (*comparator_smaller)((void *)struct1->data, (void *)struct2->data);
 }
 
-/*
-Description: Asserts that the two structs have the same data inside.
-
-Parameters:
-  - struct1 (key_tp): The first struct.
-  - struct2 (key_tp): The second struct.
-  - assignment (void (*)(const void *, const void *)): The pointer to the
-    function that assigns the data of the second struct to the data of the
-    first struct.
-
-Return values:
-    - 0: Success.
-    - 1: Failure.
-*/
 int assign(key_tp struct1,
            key_tp struct2,
            int (*assignment)(const void *, const void *)) {
@@ -151,18 +73,6 @@ struct key_int {
   int data;
 };
 
-/*
-Description: Creates a struct with its data denoted by function's parameter.
-
-Parameters:
-  - data (int): The data of the struct.
-
-Return values:
-    - key_intp: The pointer to the struct created.
-
-Asserts:
-    - malloc returns successfully.
-*/
 key_intp create_int_node(int data) {
   key_intp node = (key_intp)malloc(sizeof(struct key_int));
   assert(node != NULL);
@@ -172,33 +82,10 @@ key_intp create_int_node(int data) {
   return node;
 }
 
-/*
-Description: Prints the data of the struct with the terminal color that
-the first parameter of the function indicates.
-
-Parameters:
-  - term_color (const char *): The terminal color.
-  - struct1 (const void *): The struct to be printed.
-*/
 void int_printer(const char *term_color, const void *struct1) {
   printf("%s%d", term_color, ((key_intp)struct1)->data);
 }
 
-/*
-Description: Asserts that data in struct1 and struct2 structs are equal.
-
-Parameters:
-  - struct1 (const void *): The first struct.
-  - struct2 (const void *): The second struct.
-
-Return values:
-    - 1: The data in the two structs are equal.
-    - 0: The data in the two structs are not equal.
-
-Asserts:
-    - struct1 != NULL.
-    - struct2 != NULL.
-*/
 int int_equalizer(const void *struct1, const void *struct2) {
   assert(struct1 != NULL);
   assert(struct2 != NULL);
@@ -210,23 +97,6 @@ int int_equalizer(const void *struct1, const void *struct2) {
   }
 }
 
-/*
-Description: Asserts that data in struct1 is greater than data in struct2.
-
-Parameters:
-  - struct1 (const void *): The first struct.
-  - struct2 (const void *): The second struct.
-
-Return values:
-    - 1: The data in the first struct is greater than the data in the second
-         struct.
-    - 0: The data in the first struct is not greater than the data in the
-         second struct.
-
-Asserts:
-    - struct1 != NULL.
-    - struct2 != NULL.
-*/
 int int_comparator_greater(const void *struct1, const void *struct2) {
   assert(struct1 != NULL);
   assert(struct2 != NULL);
@@ -238,23 +108,6 @@ int int_comparator_greater(const void *struct1, const void *struct2) {
   }
 }
 
-/*
-Description: Asserts that data in struct1 is smaller than data in struct2.
-
-Parameters:
-  - struct1 (const void *): The first struct.
-  - struct2 (const void *): The second struct.
-
-Return values:
-    - 1: The data in the first struct is smaller than the data in the second
-         struct.
-    - 0: The data in the first struct is not smaller than the data in the
-         second struct.
-
-Asserts:
-    - struct1 != NULL.
-    - struct2 != NULL.
-*/
 int int_comparator_smaller(const void *struct1, const void *struct2) {
   assert(struct1 != NULL);
   assert(struct2 != NULL);
@@ -266,23 +119,6 @@ int int_comparator_smaller(const void *struct1, const void *struct2) {
   }
 }
 
-/*
-Description: Asserts that data in struct1 is smaller than data in struct2.
-
-Parameters:
-  - struct1 (const void *): The first struct.
-  - struct2 (const void *): The second struct.
-
-Return values:
-    - 1: The data in the first struct is smaller than the data in the second
-         struct.
-    - 0: The data in the first struct is not smaller than the data in the
-         second struct.
-
-Asserts:
-    - struct1 != NULL.
-    - struct2 != NULL.
-*/
 int int_assignment(const void *struct1, const void *struct2) {
   assert(struct1 != NULL);
   assert(struct2 != NULL);
--- key.h
+++ key.h
@@ -19,19 +19,103 @@ References:
 
 typedef struct key_t *key_tp;
 
+/*
+Description: Creates a struct with its data denoted by function's parameter.
+
+Parameters:
+  - data (void *): The data of the struct.
+
+Return values:
+  - key_tp: The pointer to the struct created.
+
+Asserts:
+  - malloc returns successfully.
+*/
 key_tp create_key(void *data);
+
+/*
+Description: Prints the data of the struct with the terminal color that
+the first parameter of the function indicates.
+
+Parameters:
+  - term_color (const char *): The terminal color.
+  - struct1 (key_tp): The struct to be printed.
+  - printer (void (*)(const void *)): The pointer to the function that prints
+    the data of the struct.
+*/
 void print_data(char *term_color,
                 key_tp struct1,
                 void (*printer)(const char *, const void *));
+
+/*
+Description: Asserts that data in struct1 and struct2 structs are equal.
+
+Parameters:
+  - struct1 (key_tp): The first struct.
+  - struct2 (key_tp): The second struct.
+  - equalizer (int (*)(const void *, const void *)): The pointer to the
+    function that compares the data of the two structs.
+
+Return values:
+    - 1: The data in the two structs are equal.
+    - 0: The data in the two structs are not equal.
+*/
 int are_equal(key_tp struct1,
               key_tp struct2,
               int (*equalizer)(const void *, const void *));
+
+/*
+Description: Asserts that data in struct1 is greater than data in struct2.
+
+Parameters:
+  - struct1 (key_tp): The first struct.
+  - struct2 (key_tp): The second struct.
+  - comparator_greater (int (*)(const void *, const void *)): The pointer to the
+    function that compares the data of the two structs.
+
+Return values:
+    - 1: The data in the first struct is greater than the data in the second
+         struct.
+    - 0: The data in the first struct is not greater than the data in the
+         second struct.
+*/
 int is_greater(key_tp struct1,
                key_tp struct2,
                int (*comparator_greater)(const void *, const void *));
+
+/*
+Description: Asserts that data in struct1 is smaller than data in struct2.
+
+Parameters:
+  - struct1 (key_tp): The first struct.
+  - struct2 (key_tp): The second struct.
+  - comparator_smaller (int (*)(const void *, const void *)): The pointer to the
+    function that compares the data of the two structs.
+
+Return values:
+    - 1: The data in the first struct is smaller than the data in the second
+         struct.
+    - 0: The data in the first struct is not smaller than the data in the
+         second struct.
+*/
 int is_smaller(key_tp struct1,
                key_tp struct2,
                int (*comparator_smaller)(const void *, const void *));
+
+/*
+Description: Asserts that the two structs have the same data inside.
+
+Parameters:
+  - struct1 (key_tp): The first struct.
+  - struct2 (key_tp): The second struct.
+  - assignment (void (*)(const void *, const void *)): The pointer to the
+    function that assigns the data of the second struct to the data of the
+    first struct.
+
+Return values:
+    - 0: Success.
+    - 1: Failure.
+*/
 int assign(key_tp struct1,
            key_tp struct2,
            int (*assignment)(const void *, const void *));
@@ -40,11 +124,102 @@ int assign(key_tp struct1,
 
 typedef struct key_int *key_intp;
 
+/*
+Description: Creates a struct with its data denoted by function's parameter.
+
+Parameters:
+  - data (int): The data of the struct.
+
+Return values:
+    - key_intp: The pointer to the struct created.
+
+Asserts:
+    - malloc returns successfully.
+*/
 key_intp create_int_node(int data);
+
+/*
+Description: Prints the data of the struct with the terminal color that
+the first parameter of the function indicates.
+
+Parameters:
+  - term_color (const char *): The terminal color.
+  - struct1 (const void *): The struct to be printed.
+*/
 void int_printer(const char *term_color, const void *struct1);
+
+/*
+Description: Asserts that data in struct1 and struct2 structs are equal.
+
+Parameters:
+  - struct1 (const void *): The first struct.
+  - struct2 (const void *): The second struct.
+
+Return values:
+    - 1: The data in the two structs are equal.
+    - 0: The data in the two structs are not equal.
+
+Asserts:
+    - struct1 != NULL.
+    - struct2 != NULL.
+*/
 int int_equalizer(const void *struct1, const void *struct2);
+
+/*
+Description: Asserts that data in struct1 is greater than data in struct2.
+
+Parameters:
+  - struct1 (const void *): The first struct.
+  - struct2 (const void *): The second struct.
+
+Return values:
+    - 1: The data in the first struct is greater than the data in the second
+         struct.
+    - 0: The data in the first struct is not greater than the data in the
+         second struct.
+
+Asserts:
+    - struct1 != NULL.
+    - struct2 != NULL.
+*/
 int int_comparator_greater(const void *struct1, const void *struct2);
+
+/*
+Description: Asserts that data in struct1 is smaller than data in struct2.
+
+Parameters:
+  - struct1 (const void *): The first struct.
+  - struct2 (const void *): The second struct.
+
+Return values:
+    - 1: The data in the first struct is smaller than the data in the second
+         struct.
+    - 0: The data in the first struct is not smaller than the data in the
+         second struct.
+
+Asserts:
+    - struct1 != NULL.
+    - struct2 != NULL.
+*/
 int int_comparator_smaller(const void *struct1, const void *struct2);
+
+/*
+Description: Asserts that data in struct1 is smaller than data in struct2.
+
+Parameters:
+  - struct1 (const void *): The first struct.
+  - struct2 (const void *): The second struct.
+
+Return values:
+    - 1: The data in the first struct is smaller than the data in the second
+         struct.
+    - 0: The data in the first struct is not smaller than the data in the
+         second struct.
+
+Asserts:
+    - struct1 != NULL.
+    - struct2 != NULL.
+*/
 int int_assignment(const void *struct1, const void *struct2);
 
 #endif
