28dc93b5ac46061936c22b622726b6813d599a37
Alexandros Tsagkaropoulos <alextsagas2001@gmail.com>
2024-01-11 18:28:12 +0200

    refactor code in scaffold_rbt

--- scaffold_rbt.c
+++ scaffold_rbt.c
@@ -17,26 +17,101 @@ References:
 #include "int.h"
 #include "rbt.h"
 
+/*
+Description: Prints a string separator.
+*/
+void print_separator();
+
+/*
+Description: Asks the user to choose the type of the key between
+integer and string. The choice is returned as a char.
+
+Parameters:
+  - key_type (char*): The type of the key.
+
+Return values:
+  - 'i': The user chose integer.
+  - 's': The user chose string.
+*/
+char choose_key_type();
+
+/*
+Description: Performs operations insert, delete and print on the RBT T.
+It returns when the user chooses to quit the program.
+
+Parameters:
+  - T (Treep): The RBT.
+  - key_type (char): The type of the key.
+*/
+void tree_operations(Treep T, char key_type);
+
 int main() {
   // BRT where tests are done
   Treep T = RBT_create_tree();
+
+  print_separator();
+  printf("\nCreate the RBT\n");
+  print_separator();
+
+  // Choose the type of the key
+  char key_type = choose_key_type();
+
+  // Perform operations on the RBT
+  tree_operations(T, key_type);
+
+  return 0;
+}
+
+void print_separator() {
+  printf("\n--------------------------------------------------------------\n");
+}
+
+char choose_key_type() {
+  char key_type = '_';
+
+  while (key_type != 'i' && key_type != 's') {
+    printf("Previous choice was: %c\n\n", key_type);
+    printf("Choose the type of the key:\n");
+    printf(" i: integer\n");
+    printf(" s: string\n");
+    printf("Give a new choice: ");
+
+    fflush(stdin);
+    key_type = getchar();
+    getchar();
+
+    printf("\nThe new choice is: %c", key_type);
+    print_separator();
+
+    switch (key_type) {
+    case 'i':
+      printf("\nInteger key chosen\n");
+      break;
+    case 's':
+      printf("\nString key chosen\n");
+      break;
+    default:
+      printf("\nInvalid input, please try again.\n");
+      print_separator();
+    }
+  }
+
+  return key_type;
+}
+
+void tree_operations(Treep T, char key_type) {
+  // User input
+  char answer = '_';
+  int int_key;
 
   // Helper variables
   int return_value;
   void* return_node;
   key_intp int_data;
 
-  // User input
-  char answer = '_';
-  int int_key;
-
-  printf("\n--------------------------------------------------------------\n");
-  printf("\nCreate the RBT\n");
-
   while (answer != 'q') {
-    printf(
-        "\n--------------------------------------------------------------\n");
-    printf("Previous answer was: %c\n", answer);
+    print_separator();
+    printf("Previous answer was: %c\n\n", answer);
     printf("Choose from the following options:\n");
     printf(" i: Insert an element in the RBT\n");
     printf(" d: Delete an element from the RBT\n");
@@ -48,9 +123,8 @@ int main() {
     answer = getchar();
     getchar();
 
-    printf("The new answer is: %c", answer);
-    printf(
-        "\n--------------------------------------------------------------\n");
+    printf("\nThe new answer is: %c", answer);
+    print_separator();
 
     switch (answer) {
     case 'i':
@@ -117,8 +191,6 @@ int main() {
     }
   }
 
-  printf("\n--------------------------------------------------------------\n");
+  print_separator();
   printf("\nThe program has halted.");
-
-  return 0;
 }
