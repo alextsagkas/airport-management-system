1131ae90cc49a67cf083334def0359b07acce530
Alexandros Tsagkaropoulos <alextsagas2001@gmail.com>
2024-01-11 14:00:59 +0200

    scaffold_int

--- scaffold_int.c
+++ scaffold_int.c
@@ -1,4 +1,50 @@
 #include "int.h"
 #include <stdio.h>
 
-int main() { return 0; }
+int main() {
+  key_intp int_key1 = create_int_node(1);
+  key_intp int_key2 = create_int_node(2);
+  key_intp int_key3 = create_int_node(1);
+
+  // Printer
+  printf("\nPrinter:\n--------\n");
+  int_printer("int_key1: ", (void*)int_key1);
+  printf("\n");
+  int_printer("int_key2: ", (void*)int_key2);
+  printf("\n");
+  int_printer("int_key3: ", (void*)int_key3);
+  printf("\n");
+
+  // Comparator
+  printf("\nComparator:\n-----------\n");
+  int_printer("", (void*)int_key1);
+  printf(" cmp ");
+  int_printer("", (void*)int_key2);
+  printf(" = %d\n", int_comparator((void*)int_key1, (void*)int_key2));
+
+  int_printer("", (void*)int_key2);
+  printf(" cmp ");
+  int_printer("", (void*)int_key1);
+  printf(" = %d\n", int_comparator((void*)int_key2, (void*)int_key1));
+
+  int_printer("", (void*)int_key1);
+  printf(" cmp ");
+  int_printer("", (void*)int_key3);
+  printf(" = %d\n", int_comparator((void*)int_key1, (void*)int_key3));
+
+  // Delete
+  printf("\nDelete:\n-------\n");
+  if (!delete_int_node((void*)int_key1)) {
+    printf("int_key1 was deleted successfully\n");
+  }
+
+  if (!delete_int_node((void*)int_key2)) {
+    printf("int_key2 was deleted successfully\n");
+  }
+
+  if (!delete_int_node((void*)int_key3)) {
+    printf("int_key3 was deleted successfully\n");
+  }
+
+  return 0;
+}
