fe56489266a7abe5e946097d3e8fb0de74ed7d04
Alexandros Tsagkaropoulos <alextsagas2001@gmail.com>
2024-01-12 02:07:34 +0200

    comments

--- scaffold_string.c
+++ scaffold_string.c
@@ -1,3 +1,17 @@
+/*
+author: Alexadros Tsagkaropoulos
+
+References:
+
+ - Introduction to Algorithms, Third Edition
+ by Thomas H. Cormen, Charles E. Leiserson,
+ Ronald L. Rivest and Clifford Stein.
+ - Programming Language Principles and Programming Techniques notes
+ from M.Sc. in Control and Computing taught by Ioannis Kotronis.
+ - The Practice of Computer Programming, Second Edition
+ by Brian W. Kernighan and Rob Pike.
+*/
+
 #include "string.h"
 #include <stdio.h>
 
