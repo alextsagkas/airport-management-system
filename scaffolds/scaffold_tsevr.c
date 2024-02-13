1a4e0f522682fec372430195770c07286ddfe808
Alexandros Tsagkaropoulos <alextsagas2001@gmail.com>
2024-02-14 00:35:52 +0200

    refactor .txt files

--- scaffolds/scaffold_tsevr.c
+++ scaffolds/scaffold_tsevr.c
@@ -1,8 +1,10 @@
 #include "../index/TSEvr.h"
 #include <stdio.h>
 
-#define FILEPATH "data/linux/airportsLinuxRandom.txt"
-#define NEW_FILEPATH "data/linux/test/new_airportsLinuxRandom.txt"
+// #define FILEPATH "data/linux/airportsLinuxRandom.txt"
+#define FILEPATH "data/linux/airportsLinuxSorted.txt"
+// #define NEW_FILEPATH "data/linux/test/new_airportsLinuxRandom.txt"
+#define NEW_FILEPATH "data/linux/test/new_airportsLinuxSorted.txt"
 
 int main() {
   FILE* file = fopen(FILEPATH, "r");
