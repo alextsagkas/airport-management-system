b15111c738ab6eb16ca888f24ae5672c5c555ad1
Alexandros Tsagkaropoulos <alextsagas2001@gmail.com>
2024-02-13 21:44:39 +0200

    implement tsdda module

--- index/TSDDA.c
+++ index/TSDDA.c
@@ -1,16 +1,35 @@
 #include "TSDDA.h"
 
-int TSDDA_setValue(TStoixeiouDDA* target, TStoixeiouDDA source) {}
+int TSDDA_setValue(TStoixeiouDDA* target, TStoixeiouDDA source) {
+  target->arrayIndex = source.arrayIndex;
+  target->key = source.key;
 
-int TSDDA_setKey(keyType* key, keyType Val) {}
+  return 0;
+}
 
-int TSDDA_setIndex(int* Index, int Val) {}
+int TSDDA_setKey(keyType* key, keyType Val) {
+  *key = Val;
 
-int TSDDA_equal(TStoixeiouDDA s1, TStoixeiouDDA s2) {}
+  return 0;
+}
 
-int TSDDA_smaller(TStoixeiouDDA s1, TStoixeiouDDA s2) {}
+int TSDDA_setIndex(int* Index, int Val) {
+  *Index = Val;
 
-int TSDDA_greater(TStoixeiouDDA s1, TStoixeiouDDA s2) {}
+  return 0;
+}
+
+int TSDDA_equal(TStoixeiouDDA s1, TStoixeiouDDA s2) {
+  return (s1.key == s2.key);
+}
+
+int TSDDA_smaller(TStoixeiouDDA s1, TStoixeiouDDA s2) {
+  return (s1.key < s2.key);
+}
+
+int TSDDA_greater(TStoixeiouDDA s1, TStoixeiouDDA s2) {
+  return (s1.key > s2.key);
+}
 
 /* The Following are already Implemented using Above */
 int TSDDA_smaller_or_equal(TStoixeiouDDA s1, TStoixeiouDDA s2) {
