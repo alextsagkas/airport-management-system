6199fb829cb4fb88565df89143269374d31bd7be
Alexandros Tsagkaropoulos <alextsagas2001@gmail.com>
2024-02-13 16:13:25 +0200

    minor change

--- index/TSDDA.h
+++ index/TSDDA.h
@@ -1,8 +1,8 @@
 #ifndef __TSDDA__
 #define __TSDDA__
+
 #include <stdio.h>
 
-typedef int keyType; /* integer key for airport */
 
 typedef struct KeyIndex {
   keyType key;
@@ -24,4 +24,5 @@ int TSDDA_megalytero(TStoixeiouDDA, TStoixeiouDDA);
 int TSDDA_mikrotero_iso(TStoixeiouDDA, TStoixeiouDDA);
 int TSDDA_megalytero_iso(TStoixeiouDDA, TStoixeiouDDA);
 int TSDDA_diaforo(TStoixeiouDDA, TStoixeiouDDA);
+
 #endif
